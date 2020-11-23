c59443b269b4efd0e3cec8ad51e03cb774fdb360
Ubuntu <ubuntu@gc-elated-tesla.novalocal>
2020-11-23 14:44:26 +0000

    minor degbugging

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ from haystack.retriever.dense import DensePassageRetriever
 
 # ## Preprocessing of documents
 # Let's first get some documents that we want to query
-@st.cache(allow_output_mutation=True)
+
 def create_store():
 	# FAISS is a library for efficient similarity search on a cluster of dense vectors.
 	# The FAISSDocumentStore uses a SQL(SQLite in-memory be default) document store under-the-hood
@@ -32,39 +32,35 @@ def create_store():
 	document_store.write_documents(dicts)
 	return document_store
 
-def create_retriver():
-	
-	document_store = create_store()
-	### Retriever
-	retriever = DensePassageRetriever(document_store=document_store,
-									query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-									passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
-									max_seq_len_query=64,
-									max_seq_len_passage=256,
-									batch_size=2,
-									use_gpu=True,
-									embed_title=True,
-									use_fast_tokenizers=True
-									)
 
-	# Important:
-	# Now that after we have the DPR initialized, we need to call update_embeddings() to iterate over all
-	# previously indexed documents and update their embedding representation.
-	# While this can be a time consuming operation (depending on corpus size), it only needs to be done once.
-	# At query time, we only need to embed the query and compare it the existing doc embeddings which is very fast.
-	document_store.update_embeddings(retriever)
+document_store = create_store()
+### Retriever
+retriever = DensePassageRetriever(document_store=document_store,
+								query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+								passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
+								max_seq_len_query=64,
+								max_seq_len_passage=256,
+								batch_size=2,
+								use_gpu=True,
+								embed_title=True,
+								use_fast_tokenizers=True
+								)
 
-	### Reader
-	# Load a  local model or any of the QA models on
-	# Hugging Face's model hub (https://huggingface.co/models)
-	reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+# Important:
+# Now that after we have the DPR initialized, we need to call update_embeddings() to iterate over all
+# previously indexed documents and update their embedding representation.
+# While this can be a time consuming operation (depending on corpus size), it only needs to be done once.
+# At query time, we only need to embed the query and compare it the existing doc embeddings which is very fast.
+document_store.update_embeddings(retriever)
 
-	### Finder
-	# The Finder sticks together reader and retriever in a pipeline to answer our actual questions.
-	finder = Finder(reader, retriever)
+### Reader
+# Load a  local model or any of the QA models on
+# Hugging Face's model hub (https://huggingface.co/models)
+reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
 
-	return finder
-
+### Finder
+# The Finder sticks together reader and retriever in a pipeline to answer our actual questions.
+finder = Finder(reader, retriever)
 
 
 #prediction = finder.get_answers(question="Who is the father of Arya Stark?", top_k_retriever=10, top_k_reader=5)
@@ -73,7 +69,7 @@ def create_retriver():
 # prediction = finder.get_answers(question="Who created the Dothraki vocabulary?", top_k_reader=5)
 # prediction = finder.get_answers(question="Who is the sister of Sansa?", top_k_reader=5)
 
-finder = create_retriver()
+#finder = create_retriver()
 st.title('My first app')
 
 # Start sidebar
@@ -160,10 +156,12 @@ else:
 
 if st.button("Show me!"):
 	if action in [0, 1, 2]:
-		prediction = finder.get_answers(question=question, top_k_retriever=10, top_k_reader=5)
+		prediction = finder.get_answers(question=str(question), top_k_retriever=10, top_k_reader=5)
 		answers = [prediction['answers'][i]['answer'] for i in range(len(prediction['answers']))]
 		contexts = [prediction['answers'][i]['context'] for i in range(len(prediction['answers']))]
-	
+
+		#answers =["sg","SDgsdg"]
+		#contexts = ["sg","SDgsdg"]
 	if action in [0, 2]:
 		
 		st.markdown("### The model generated answer is:")
