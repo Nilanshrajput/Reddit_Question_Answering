48f51e25bde5d8403e867154bc16ea633ec9998e
Nilanshrajput <2017csb1092@iitrpr.ac.in>
2020-11-23 15:09:44 +0000

    debug

--- src/app.py
+++ src/app.py
@@ -9,11 +9,14 @@ from haystack.preprocessor.utils import convert_files_to_dicts, fetch_archive_fr
 from haystack.reader.farm import FARMReader
 from haystack.utils import print_answers
 from haystack.retriever.dense import DensePassageRetriever
-
+import torch
+import transformers
 
 # ## Preprocessing of documents
 # Let's first get some documents that we want to query
-
+@st.cache(
+    
+)
 def create_store():
 	# FAISS is a library for efficient similarity search on a cluster of dense vectors.
 	# The FAISSDocumentStore uses a SQL(SQLite in-memory be default) document store under-the-hood
@@ -30,37 +33,40 @@ def create_store():
 
 	# Now, let's write the docs to our DB.
 	document_store.write_documents(dicts)
-	return document_store
+	
 
 
-document_store = create_store()
-### Retriever
-retriever = DensePassageRetriever(document_store=document_store,
-								query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-								passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
-								max_seq_len_query=64,
-								max_seq_len_passage=256,
-								batch_size=2,
-								use_gpu=True,
-								embed_title=True,
-								use_fast_tokenizers=True
-								)
+	document_store = create_store()
+	### Retriever
+	retriever = DensePassageRetriever(document_store=document_store,
+									query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+									passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
+									max_seq_len_query=64,
+									max_seq_len_passage=256,
+									batch_size=2,
+									use_gpu=True,
+									embed_title=True,
+									use_fast_tokenizers=True
+									)
 
-# Important:
-# Now that after we have the DPR initialized, we need to call update_embeddings() to iterate over all
-# previously indexed documents and update their embedding representation.
-# While this can be a time consuming operation (depending on corpus size), it only needs to be done once.
-# At query time, we only need to embed the query and compare it the existing doc embeddings which is very fast.
-document_store.update_embeddings(retriever)
+	# Important:
+	# Now that after we have the DPR initialized, we need to call update_embeddings() to iterate over all
+	# previously indexed documents and update their embedding representation.
+	# While this can be a time consuming operation (depending on corpus size), it only needs to be done once.
+	# At query time, we only need to embed the query and compare it the existing doc embeddings which is very fast.
+	document_store.update_embeddings(retriever)
 
-### Reader
-# Load a  local model or any of the QA models on
-# Hugging Face's model hub (https://huggingface.co/models)
-reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+	### Reader
+	# Load a  local model or any of the QA models on
+	# Hugging Face's model hub (https://huggingface.co/models)
+	reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
 
-### Finder
-# The Finder sticks together reader and retriever in a pipeline to answer our actual questions.
-finder = Finder(reader, retriever)
+	### Finder
+	# The Finder sticks together reader and retriever in a pipeline to answer our actual questions.
+	finder = Finder(reader, retriever)
+	return finder
+
+finder = create_store()
 
 
 #prediction = finder.get_answers(question="Who is the father of Arya Stark?", top_k_retriever=10, top_k_reader=5)
